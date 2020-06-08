697880f8c729578532f6f0b339ef4797496cc05a
Shane Gilbert <sdgilber@ucsd.edu>
2020-06-07 19:32:15 -0700

    All operators work except equals

--- calculator.js
+++ calculator.js
@@ -9,16 +9,17 @@ TODO:
 5. changing of operators befor einputting numbers
 */
 
-let numbers = document.getElementsByClassName("numbers");
-let buttons = document.getElementsByTagName("button");
-
+//Stores numeric inputs
 let inputArray = [];
-if(inputArray = []) {
-    document.querySelector("#clear").innerHTML = "CE";
-}
-
+//Current numeric input
 let inputNumber = "";
+//Stores operator selection
+let operator = "";
 
+/**
+ * Handles the different buttons and their associated functionality (number/operator)
+ * @param {String} clicked_id the html id associated with each button
+ */
 function buttonPress(clicked_id) {
     switch(clicked_id) {
         case "clear":
@@ -27,40 +28,155 @@ function buttonPress(clicked_id) {
             if(inputNumber == "") {
                 document.querySelector("#clear").innerHTML = "CE";
             }
-            if(document.querySelector("#clear").innerHTML = "CE") {
+            if(document.querySelector("#clear").innerHTML == "CE") {
                 inputArray = [];
+                operator = "";
             }
-            console.log("clear");
             break;
         
         case "del":
             inputNumber = inputNumber.substring(0, inputNumber.length-1);
             document.querySelector(".numbersBox").innerHTML = inputNumber;
+            if(inputNumber == "") {
+                document.querySelector("#clear").innerHTML = "CE";
+            }
             break;
         
         case "div":
-            inputArray.push(inputNumber);
-            inputNumber = "";
-            inputArray.push("div");
+            if (operator == "") {               //Functions when there is no previous operator
+                if (inputNumber !== "") {       //Saves the input and operator after first input(number then operator)
+                    operator = "div";
+                    inputArray.push(inputNumber);
+                    inputNumber = "";
+                } else if (inputNumber == "") { //Repetition of operator when there is no input
+                    break;
+                }
+            } else if (operator == "div") {     //Functions with selected operator   
+                if (inputArray.length == 1 && inputNumber !== "") {             //Repetition of the operator when there is a previous and current input (operates)
+                    inputArray.push(inputNumber);
+                    let result = operate(operator);
+                    document.querySelector(".numbersBox").innerHTML = result;
+                    inputArray[0] = result;
+                    inputArray.pop();
+                    inputNumber = "";
+                }   else if (inputArray.length == 1 && inputNumber == "") {     //Repetition of the operator when there is no current input (does nothing)
+                    break;
+                }
+            } else if (operator !== "div" && inputNumber == "") {               //Changes the operator to div when it was previously something else
+                operator = "div";
+                break;
+            } else if (operator !== "div" && inputNumber !== "") {              //operates (previous operator) when there is a current input and changes the operator to the new selected operator
+                inputArray.push(inputNumber);
+                let result = operate(operator);
+                document.querySelector(".numbersBox").innerHTML = result;
+                inputArray[0] = result;
+                inputArray.pop();
+                operator = "div";
+                inputNumber = "";
+            }
             break;
         
         case "mult":
-            inputArray.push(inputNumber);
-            inputNumber = "";
-            inputArray.push("mult");
+            if (operator == "") {               //Functions when there is no previous operator
+                if (inputNumber !== "") {       //Saves the input and operator after first input(number then operator)
+                    operator = "mult";
+                    inputArray.push(inputNumber);
+                    inputNumber = "";
+                } else if (inputNumber == "") { //Repetition of operator when there is no input
+                    break;
+                }
+            } else if (operator == "mult") {     //Functions with selected operator   
+                if (inputArray.length == 1 && inputNumber !== "") {             //Repetition of the operator when there is a previous and current input (operates)
+                    inputArray.push(inputNumber);
+                    let result = operate(operator);
+                    document.querySelector(".numbersBox").innerHTML = result;
+                    inputArray[0] = result;
+                    inputArray.pop();
+                    inputNumber = "";
+                }   else if (inputArray.length == 1 && inputNumber == "") {     //Repetition of the operator when there is no current input (does nothing)
+                    break;
+                }
+            } else if (operator !== "mult" && inputNumber == "") {               //Changes the operator to mult when it was previously something else
+                operator = "mult";
+                break;
+            } else if (operator !== "mult" && inputNumber !== "") {              //operates (previous operator) when there is a current input and changes the operator to the new selected operator
+                inputArray.push(inputNumber);
+                let result = operate(operator);
+                document.querySelector(".numbersBox").innerHTML = result;
+                inputArray[0] = result;
+                inputArray.pop();
+                operator = "mult";
+                inputNumber = "";
+            }
             break;
         
         case "sub":
-            inputArray.push(inputNumber);
-            inputNumber = "";
-            inputArray.push("sub");
+            if (operator == "") {               //Functions when there is no previous operator
+                if (inputNumber !== "") {       //Saves the input and operator after first input(number then operator)
+                    operator = "sub";
+                    inputArray.push(inputNumber);
+                    inputNumber = "";
+                } else if (inputNumber == "") { //Repetition of operator when there is no input
+                    break;
+                }
+            } else if (operator == "sub") {     //Functions with selected operator   
+                if (inputArray.length == 1 && inputNumber !== "") {             //Repetition of the operator when there is a previous and current input (operates)
+                    inputArray.push(inputNumber);
+                    let result = operate(operator);
+                    document.querySelector(".numbersBox").innerHTML = result;
+                    inputArray[0] = result;
+                    inputArray.pop();
+                    inputNumber = "";
+                }   else if (inputArray.length == 1 && inputNumber == "") {     //Repetition of the operator when there is no current input (does nothing)
+                    break;
+                }
+            } else if (operator !== "sub" && inputNumber == "") {               //Changes the operator to sub when it was previously something else
+                operator = "sub";
+                break;
+            } else if (operator !== "sub" && inputNumber !== "") {              //operates (previous operator) when there is a current input and changes the operator to the new selected operator
+                inputArray.push(inputNumber);
+                let result = operate(operator);
+                document.querySelector(".numbersBox").innerHTML = result;
+                inputArray[0] = result;
+                inputArray.pop();
+                operator = "sub";
+                inputNumber = "";
+            }
             break;
-        
+
         case "add":
-            inputArray.push(inputNumber);
-            inputNumber = "";
-            inputArray.push("add");
-            break;
+            if (operator == "") {               //Functions when there is no previous operator
+                if (inputNumber !== "") {       //Saves the input and operator after first input(number then operator)
+                    operator = "add";
+                    inputArray.push(inputNumber);
+                    inputNumber = "";
+                } else if (inputNumber == "") { //Repetition of operator when there is no input
+                    break;
+                }
+            } else if (operator == "add") {     //Functions with selected operator   
+                if (inputArray.length == 1 && inputNumber !== "") {             //Repetition of the operator when there is a previous and current input (operates)
+                    inputArray.push(inputNumber);
+                    let result = operate(operator);
+                    document.querySelector(".numbersBox").innerHTML = result;
+                    inputArray[0] = result;
+                    inputArray.pop();
+                    inputNumber = "";
+                }   else if (inputArray.length == 1 && inputNumber == "") {     //Repetition of the operator when there is no current input (does nothing)
+                    break;
+                }
+            } else if (operator !== "add" && inputNumber == "") {               //Changes the operator to add when it was previously something else
+                operator = "add";
+                break;
+            } else if (operator !== "add" && inputNumber !== "") {              //operates (previous operator) when there is a current input and changes the operator to the new selected operator
+                inputArray.push(inputNumber);
+                let result = operate(operator);
+                document.querySelector(".numbersBox").innerHTML = result;
+                inputArray[0] = result;
+                inputArray.pop();
+                operator = "add";
+                inputNumber = "";
+            }
+        break;
         
         case "equal":
             inputArray.push(inputNumber);
@@ -166,7 +282,7 @@ function buttonPress(clicked_id) {
  * @result the sum of num1 and num2
  */
 function add(num1, num2) {
-    return num1 + num2;
+    return parseInt(num1) + parseInt(num2);
 }
 
 /**
@@ -197,4 +313,22 @@ function mult(num1, num2) {
  */
 function div(num1, num2) {
     return num1 / num2;
+}
+
+/**
+ * Handles different operator calls
+ * @param {String} operator 
+ * @return the result of the operation
+ */
+function operate(operator) {
+    switch(operator) {
+        case "add": 
+            return add(inputArray[0], inputArray[1]);
+        case "sub": 
+            return sub(inputArray[0], inputArray[1]);
+        case "div":
+            return div(inputArray[0], inputArray[1]);
+        case "mult":
+            return mult(inputArray[0], inputArray[1]);
+    }
 }
